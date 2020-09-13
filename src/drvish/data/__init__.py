30bd7da10a04236d0f902aebb984a53ce63a4a20
James Webber <james.webber@czbiohub.org>
2020-09-12 17:01:12 -0700

    wip

--- src/drvish/data/__init__.py
+++ src/drvish/data/__init__.py
@@ -58,13 +58,19 @@ class DataTargetLoader(DataLoader):
             yield batch
 
 
-def split_dataset(*xs: torch.Tensor, batch_size: int, train_p: float):
+def split_dataset(
+    *xs: torch.Tensor,
+    batch_size: int,
+    train_p: float,
+    dataloader_cls: Type[DataLoader] = DataLoader,
+):
     """
     Split a dataset of tensors into training and validation sets with a given split.
 
     :param xs: tensor(s) of data to split into two parts
     :param batch_size: number of samples to provide in a single iteration
     :param train_p: proportion of the data to put in the training set
+    :param dataloader_cls: class constructor for data loader
     :return: two DataLoaders, one for training and another for validation
     """
     n_cells = xs[0].shape[0]
@@ -74,13 +80,13 @@ def split_dataset(*xs: torch.Tensor, batch_size: int, train_p: float):
 
     dataset = TensorDataset(*xs)
 
-    data_loader_train = DataLoader(
+    data_loader_train = dataloader_cls(
         dataset=dataset,
         batch_size=batch_size,
         sampler=SubsetRandomSampler(example_indices[:n_train]),
     )
 
-    data_loader_validation = DataLoader(
+    data_loader_validation = dataloader_cls(
         dataset=dataset,
         batch_size=batch_size,
         sampler=SubsetRandomSampler(example_indices[n_train:]),
@@ -108,6 +114,7 @@ def split_labeled_dataset(
     :return: two DataLoaders, one for training and another for validation
     """
     n_cells = xs[0].shape[0]
+
     example_indices = np.random.permutation(n_cells)
     example_labels = labels[example_indices]
     n_train = int(train_p * n_cells)
---
+++ src/drvish/models/mcv_nbvae.py
@@ -0,0 +1,107 @@
+#!/usr/bin/env python
+
+
+from typing import Sequence
+
+import pyro
+import pyro.distributions as dist
+import pyro.poutine as poutine
+import torch
+import torch.nn as nn
+
+from drvish.models.modules import Encoder, NBDecoder, _normal_prior
+
+
+class NBVAE(nn.Module):
+    r"""Variational auto-encoder model with negative binomial loss.
+
+    :param n_input: Number of input genes
+    :param n_latent: Dimensionality of the latent space
+    :param layers: Number and size of hidden layers used for encoder and decoder NNs
+    :param lib_loc: Mean for prior distribution on library scaling factor
+    :param lib_scale: Scale for prior distribution on library scaling factor
+    :param dropout_rate: Dropout rate for neural networks
+    :param use_cuda: if True, copy parameters into GPU memory
+    :param eps: value to add to NB count parameter for numerical stability
+    """
+
+    def __init__(
+        self,
+        *,
+        n_input: int,
+        n_latent: int = 8,
+        layers: Sequence[int] = (64, 64, 64),
+        lib_loc: float = 7.5,
+        lib_scale: float = 0.5,
+        dropout_rate: float = 0.1,
+        use_cuda: bool = False,
+        eps: float = 1e-6,
+    ):
+        super().__init__()
+        self.encoder = Encoder(n_input, n_latent, layers, dropout_rate)
+        self.l_encoder = Encoder(n_input, 1, layers, dropout_rate)
+        self.decoder = NBDecoder(n_latent, n_input, layers[::-1], dropout_rate)
+
+        self.eps = torch.tensor(eps, requires_grad=False)
+
+        self.z_prior = _normal_prior(0.0, 1.0, 1, n_latent, use_cuda=use_cuda)
+        self.l_prior = _normal_prior(lib_loc, lib_scale, 1, 1, use_cuda=use_cuda)
+
+        if use_cuda:
+            # calling cuda() here will put all the parameters of
+            # the encoder and decoder networks into gpu memory
+            self.cuda()
+
+        self.use_cuda = use_cuda
+        self.n_latent = n_latent
+
+    def model(self, x: torch.Tensor, af: torch.Tensor):
+        # register PyTorch module `decoder` with Pyro
+        pyro.module("decoder", self.decoder)
+
+        with pyro.plate("data"):
+            with poutine.scale(scale=af):
+                z = pyro.sample(
+                    "latent",
+                    self.z_prior.expand(
+                        torch.Size((x.size(0), self.n_latent))
+                    ).to_event(1),
+                )
+                l = pyro.sample(
+                    "library",
+                    self.l_prior.expand(torch.Size((x.size(0), 1))).to_event(1),
+                )
+
+            log_r, logit = self.decoder(z, l)
+
+            pyro.sample(
+                "obs",
+                dist.NegativeBinomial(
+                    total_count=torch.exp(log_r) + self.eps,
+                    logits=logit,
+                    validate_args=True,
+                ).to_event(1),
+                obs=x,
+            )
+
+    # define the guide (i.e. variational distribution) q(z|x)
+    def guide(self, x: torch.Tensor, af: torch.Tensor):
+        # register PyTorch module `encoder` with Pyro
+        pyro.module("encoder", self.encoder)
+        pyro.module("l_encoder", self.l_encoder)
+
+        with pyro.plate("data"):
+            # use the encoder to get the parameters used to define q(z|x)
+            z_loc, z_scale = self.encoder(x)
+            l_loc, l_scale = self.l_encoder(x)
+
+            # sample the latent code z
+            with poutine.scale(scale=af):
+                pyro.sample(
+                    "latent",
+                    dist.Normal(z_loc, z_scale, validate_args=True).to_event(1),
+                )
+                pyro.sample(
+                    "library",
+                    dist.Normal(l_loc, l_scale, validate_args=True).to_event(1),
+                )
